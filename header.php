d183651368f080e165bb3943e44939698503ff63
Przemysław Bąchorek <p.bachorek@red-dog.pl>
2025-10-03 12:26:34 +0200

    Dodano klasy WordPressa do tagu body

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
 	<?php get_template_part( 'template-parts/menu', 'skip-links' ); ?>
 
