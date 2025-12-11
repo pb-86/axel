0d36a7de9a11d03e39e447d05398d677cd84f8f6
Przemysław Bąchorek <p.bachorek@red-dog.pl>
2025-12-11 16:40:33 +0100

    Dodano: ładowanie pliku ze skryptami JS

--- config/scripts.php
+++ config/scripts.php
@@ -7,5 +7,15 @@
  * @author Reddog Systems <p.bachorek@red-dog.pl>
  */
 
-// Adres folderu, w którym znajdują się pliki skryptów JS.
-define( 'THEME_SCRIPTS_DIR', get_template_directory_uri() . '/assets/scripts/' );
+/**
+ * Stała zawierająca adres folderu, w którym znajdują się skrrypty JS.
+ */
+define( 'THEME_SCRIPTS_DIR', get_stylesheet_directory_uri() . '/assets/scripts/' );
+
+/**
+ * Ładuje zadane skrypty JS.
+ */
+function get_theme_scripts() {
+	wp_enqueue_script( 'master', esc_url( THEME_SCRIPTS_DIR . 'master.min.js' ), array(), THEME_VERSION, true );
+}
+add_action( 'wp_enqueue_scripts', 'get_theme_scripts' );
