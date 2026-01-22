534f6887619c9de6137d87ac3b4ea49dd917eb64
Przemysław Bąchorek <p.bachorek@red-dog.pl>
2026-01-22 16:03:41 +0100

    Dodano: szablon strony z błędem 404

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Szablon strony błędu 404.
+ *
+ * @package WordPress
+ * @subpackage Axel
+ * @author Reddog Systems <p.bachorek@red-dog.pl>
+ */
+
+get_header();
+?>
+
+<main class="main" id="main" tabindex="-1">
+
+	<article>
+
+		<!--Tytuł-->
+		<h1>Strona nie została znaleziona</h1>
+
+		<!--Treść strony-->
+		<p>Jeśli wpisałeś adres tej strony ręcznie, sprawdź, czy jest poprawny.</p>
+		<p>Jeśli wkleiłeś ten adres, sprawdź czy skopiowałeś go w całości.</p>
+
+	</article>
+
+</main>
+
+<?php get_footer(); ?>
